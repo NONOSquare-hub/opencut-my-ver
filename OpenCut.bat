a8c0bd6d28eb8f05ce9db458ebd8a7b0aa9c6ba4
NONOSquare-hub <traviet1990@gmail.com>
2026-08-05 20:17:33 +0700

    fix: resolve powershell environment variable resolution error by using CMD %TEMP%

--- OpenCut.bat
+++ OpenCut.bat
@@ -75,7 +75,7 @@ if "%HAS_VC_REDIST%"=="0" (
     echo     Vui long chon 'Yes' khi he thong hoi quyen Administrator UAC.
     echo.
     
-    powershell -NoProfile -ExecutionPolicy Bypass -Command "Invoke-WebRequest -Uri 'https://aka.ms/vs/17/release/vc_redist.x64.exe' -OutFile '$env:TEMP\vc_redist.x64.exe'; Start-Process -FilePath '$env:TEMP\vc_redist.x64.exe' -ArgumentList '/install /passive /norestart' -Verb RunAs -Wait"
+    powershell -NoProfile -ExecutionPolicy Bypass -Command "Invoke-WebRequest -Uri 'https://aka.ms/vs/17/release/vc_redist.x64.exe' -OutFile '%TEMP%\vc_redist.x64.exe'; Start-Process -FilePath '%TEMP%\vc_redist.x64.exe' -ArgumentList '/install /passive /norestart' -Verb RunAs -Wait"
     
     echo.
     echo ============================================================
